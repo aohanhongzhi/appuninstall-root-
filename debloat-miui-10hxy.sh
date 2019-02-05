4d71ca1cf726ffa0cf2aa47774584cc3a8471ad5
aohanhongzhi <aohanhongzhi@qq.com>
2019-02-05 22:40:14 +0800

    first commit

---
+++ debloat-miui-10hxy.sh
@@ -0,0 +1,145 @@
+# 使用-k参数：保留安装包的数据和缓存目录。
+adb shell pm uninstall -k --user 0 com.android.bips
+adb shell pm uninstall -k --user 0 com.android.bookmarkprovider
+adb shell pm uninstall -k --user 0 com.android.browser
+adb shell pm uninstall -k --user 0 com.android.egg
+adb shell pm uninstall -k --user 0 com.android.soundrecorder
+adb shell pm uninstall -k --user 0 com.autonavi.minimap
+adb shell pm uninstall -k --user 0 com.baidu.duersdk.opensdk
+adb shell pm uninstall -k --user 0 com.baidu.input_mi
+adb shell pm uninstall -k --user 0 com.baidu.searchbox
+adb shell pm uninstall -k --user 0 com.caf.fmradio
+adb shell pm uninstall -k --user 0 com.duokan.phone.remotecontroller
+adb shell pm uninstall -k --user 0 com.eterno
+adb shell pm uninstall -k --user 0 com.facebook.appmanager
+adb shell pm uninstall -k --user 0 com.facebook.katana
+adb shell pm uninstall -k --user 0 com.facebook.services
+adb shell pm uninstall -k --user 0 com.facebook.system
+adb shell pm uninstall -k --user 0 com.google.android.apps.docs
+adb shell pm uninstall -k --user 0 com.google.android.apps.tachyon
+adb shell pm uninstall -k --user 0 com.google.android.music
+adb shell pm uninstall -k --user 0 com.google.android.tts
+adb shell pm uninstall -k --user 0 com.google.android.videos
+adb shell pm uninstall -k --user 0 com.kikaoem.xiaomi.qisiemoji.inputmethod
+adb shell pm uninstall -k --user 0 com.mfashiongallery.emag
+adb shell pm uninstall -k --user 0 com.mi.android.globalFileexplorer
+adb shell pm uninstall -k --user 0 com.mi.android.globalpersonalassistant
+adb shell pm uninstall -k --user 0 com.microsoft.office.excel
+adb shell pm uninstall -k --user 0 com.microsoft.office.outlook
+adb shell pm uninstall -k --user 0 com.microsoft.office.powerpoint
+adb shell pm uninstall -k --user 0 com.microsoft.office.word
+adb shell pm uninstall -k --user 0 com.mi.global.bbs
+adb shell pm uninstall -k --user 0 com.mi.global.shop
+adb shell pm uninstall -k --user 0 com.mi.globalTrendNews
+adb shell pm uninstall -k --user 0 com.milink.service
+adb shell pm uninstall -k --user 0 com.mi.liveassistant
+adb shell pm uninstall -k --user 0 com.miui.analytics
+adb shell pm uninstall -k --user 0 com.miui.android.fashiongallery
+adb shell pm uninstall -k --user 0 com.miui.bugreport
+adb shell pm uninstall -k --user 0 com.miui.cloudbackup
+adb shell pm uninstall -k --user 0 com.miui.cloudservice
+adb shell pm uninstall -k --user 0 com.miui.cloudservice.sysbase
+adb shell pm uninstall -k --user 0 com.miui.compass
+adb shell pm uninstall -k --user 0 com.miui.contentcatcher
+adb shell pm uninstall -k --user 0 com.miui.daemon
+adb shell pm uninstall -k --user 0 com.miui.fm
+adb shell pm uninstall -k --user 0 com.miui.hybrid
+adb shell pm uninstall -k --user 0 com.miui.hybrid.accessory
+adb shell pm uninstall -k --user 0 com.miui.klo.bugreport
+adb shell pm uninstall -k --user 0 com.miui.personalassistant
+adb shell pm uninstall -k --user 0 com.miui.player
+adb shell pm uninstall -k --user 0 com.miui.systemAdSolution
+adb shell pm uninstall -k --user 0 com.miui.translation.kingsoft
+adb shell pm uninstall -k --user 0 com.miui.translationservice
+adb shell pm uninstall -k --user 0 com.miui.translation.youdao
+adb shell pm uninstall -k --user 0 com.miui.video
+adb shell pm uninstall -k --user 0 com.miui.videoplayer
+adb shell pm uninstall -k --user 0 com.miui.videoplayer.overlay
+adb shell pm uninstall -k --user 0 com.miui.virtualsim
+adb shell pm uninstall -k --user 0 com.miui.vsimcore
+adb shell pm uninstall -k --user 0 com.miui.yellowpage
+adb shell pm uninstall -k --user 0 com.mi.webkit.core
+adb shell pm uninstall -k --user 0 com.netflix.mediaclient
+adb shell pm uninstall -k --user 0 com.netflix.partner.activation
+adb shell pm uninstall -k --user 0 com.opera.app.news
+adb shell pm uninstall -k --user 0 com.opera.branding
+adb shell pm uninstall -k --user 0 com.opera.branding.news
+adb shell pm uninstall -k --user 0 com.opera.mini.native
+adb shell pm uninstall -k --user 0 com.phonepe.app
+adb shell pm uninstall -k --user 0 com.qiyi.video
+adb shell pm uninstall -k --user 0 com.skype.m2
+adb shell pm uninstall -k --user 0 com.sohu.inputmethod.sogou.xiaomi
+adb shell pm uninstall -k --user 0 com.til.timesnews
+adb shell pm uninstall -k --user 0 com.UCMobile.intl
+adb shell pm uninstall -k --user 0 com.xiaomi.ab
+adb shell pm uninstall -k --user 0 com.xiaomi.channel
+adb shell pm uninstall -k --user 0 com.xiaomi.gamecenter.sdk.service
+adb shell pm uninstall -k --user 0 com.xiaomi.glgm
+adb shell pm uninstall -k --user 0 com.xiaomi.joyose
+adb shell pm uninstall -k --user 0 com.xiaomi.jr
+adb shell pm uninstall -k --user 0 com.xiaomi.lens
+adb shell pm uninstall -k --user 0 com.xiaomi.midrop
+adb shell pm uninstall -k --user 0 com.xiaomi.mipicks
+adb shell pm uninstall -k --user 0 com.xiaomi.mirecycle
+adb shell pm uninstall -k --user 0 com.xiaomi.o2o
+adb shell pm uninstall -k --user 0 com.xiaomi.oversea.ecom
+adb shell pm uninstall -k --user 0 com.xiaomi.pass
+adb shell pm uninstall -k --user 0 com.xiaomi.payment
+adb shell pm uninstall -k --user 0 com.xiaomi.scanner
+adb shell pm uninstall -k --user 0 com.xiaomi.shop
+adb shell pm uninstall -k --user 0 com.xiaomi.vipaccount
+
+#add
+adb shell pm uninstall -k --user 0 com.miui.calculator
+adb shell pm uninstall -k --user 0 com.android.email
+adb shell pm uninstall -k --user 0 com.xiaomi.gamecenter
+adb shell pm uninstall -k --user 0 com.xiaomi.mifisecurity
+adb shell pm uninstall -k --user 0 com.xiaomi.mimobile.noti
+adb shell pm uninstall -k --user 0 com.android.midrive
+adb shell pm uninstall -k --user 0 com.android.providers.userdictionary
+adb shell pm uninstall -k --user 0 com.android.musicfx
+adb shell pm uninstall -k --user 0 com.android.stk
+adb shell pm uninstall -k --user 0 com.miui.greenguard
+adb shell pm uninstall -k --user 0 com.android.dreams.basic
+adb shell pm uninstall -k --user 0 com.miui.userguide
+adb shell pm uninstall -k --user 0 com.android.dreams.phototable
+adb shell pm uninstall -k --user 0 com.android.htmlviewer
+adb shell pm uninstall -k --user 0 com.android.wallpaper.livepicker
+adb shell pm uninstall -k --user 0 com.google.android.marvin.talkback
+adb shell pm uninstall -k --user 0 com.android.apps.tag
+adb shell pm uninstall -k --user 0 com.android.printspooler
+adb shell pm uninstall -k --user 0 com.android.keychain
+
+#data for new phone
+
+adb shell pm uninstall -k --user 0 com.hipu.yidian
+adb shell pm uninstall -k --user 0 ctrip.android.view
+adb shell pm uninstall -k --user 0 com.xiaomi.youpin
+adb shell pm uninstall -k --user 0 com.wali.live
+adb shell pm uninstall -k --user 0 com.sankuai.meituan
+adb shell pm uninstall -k --user 0 com.sina.weibo
+adb shell pm uninstall -k --user 0 com.achievo.vipshop
+adb shell pm uninstall -k --user 0 com.suning.mobile.ebuy
+adb shell pm uninstall -k --user 0 com.wuba
+adb shell pm uninstall -k --user 0 com.miui.miuibbs
+adb shell pm uninstall -k --user 0 com.iflytek.inputmethod.miui
+
+
+#failure
+adb shell pm uninstall -k --user 0 com.mfashiongallery.emag
+
+
+
+#reserve
+# adb shell pm uninstall -k --user 0 com.android.calendar
+# adb shell pm uninstall -k --user 0 com.mipay.wallet
+# adb shell pm uninstall -k --user 0 com.mipay.wallet.in
+# adb shell pm uninstall -k --user 0 com.miui.weather2
+# adb shell pm uninstall -k --user 0 com.miui.screenrecorder
+# adb shell pm uninstall -k --user 0 com.miui.gallery
+# adb shell pm uninstall -k --user 0 com.miui.cleanmaster
+
+# 小米商店不可以删除，否则会卡米，之后进入recovery
+# adb shell pm uninstall -k --user 0 com.xiaomi.market
+# reboot
+adb reboot
