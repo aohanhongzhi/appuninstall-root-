7c07a7d89862bbc6306d23a498436052055f382a
aohanhongzhi <37319319+aohanhongzhi@users.noreply.github.com>
2021-08-03 23:12:23 +0800

    卸载红米k40的MIUI12.5一些垃圾应用
    已知问题：卸载视频之后，小米天气里面的视频无法播放，不过不影响天气应用运行。

---
+++ uninstall.sh
@@ -0,0 +1,35 @@
+# 卸载MIUI 12.5
+adb shell pm uninstall -k --user  0 com.android.browser
+adb shell pm uninstall -k --user 0  com.android.browser
+adb shell pm uninstall -k --user 0  com.miui.miservice
+adb shell pm uninstall -k --user 0  com.miui.calculator
+adb shell pm uninstall -k --user 0  com.miui.compass
+adb shell pm uninstall -k --user 0  com.miui.yellowpage
+adb shell pm uninstall -k --user 0  com.android.browser
+adb shell pm uninstall -k --user 0  com.miui.video
+adb shell pm uninstall -k --user 0  com.sohu.inputmethod.sogou.xiaomi
+
+adb shell pm uninstall -k --user 0  com.miui.miservice
+adb shell pm uninstall -k --user 0  com.android.providers.userdictionary
+# 下面这个卸载之后，无法打开设置-连接与共享
+# adb shell pm uninstall -k --user 0  com.google.android.gms 
+adb shell pm uninstall -k --user 0  com.android.stk
+adb shell pm uninstall -k --user 0  com.xiaomi.ab
+
+adb shell pm uninstall -k --user 0  com.miui.bugreport
+adb shell pm uninstall -k --user 0  com.google.android.gsf
+adb shell pm uninstall -k --user 0  com.android.vending
+adb shell pm uninstall -k --user 0  com.google.android.gsf
+adb shell pm uninstall -k --user 0  com.miui.bugreport
+adb shell pm uninstall -k --user 0  com.xiaomi.ab
+adb shell pm uninstall -k --user 0  com.android.stk
+adb shell pm uninstall -k --user 0  com.miui.newhome
+adb shell pm uninstall -k --user 0  com.miui.phrase
+adb shell pm uninstall -k --user 0  com.android.providers.userdictionar
+adb shell pm uninstall -k --user 0  com.miui.fmservice
+adb shell pm uninstall -k --user 0  com.unionpay.tsmservice.mi
+adb shell pm uninstall -k --user 0  com.android.carrierdefaultapp
+adb shell pm uninstall -k --user 0  com.miui.hybird.accessory
+adb shell pm uninstall -k --user 0  com.miui.smarttravel
+adb shell pm uninstall -k --user 0  com.android.apps.tah
+adb shell pm uninstall -k --user 0  com.android.apps.tag
